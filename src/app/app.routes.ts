f9f78346806e7168107db1af975dc4ed64f7d502
Muhammad Iftikhar <mohammadiftikharjoiya@gmail.com>
2024-12-30 18:11:29 +0100

    new components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SearchComponent } from './main/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SignupComponent},
+    { path: 'signin', component: SigninComponent},
+    { path: 'searchbypostalcode', component: SearchComponent},
+];
